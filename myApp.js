b8e752850feb3c43786a42c9ffe554065fde235f
jackkershaw <hello@jackkershaw.net>
2025-05-30 15:59:19 +0100

    doing challenges

--- myApp.js
+++ myApp.js
@@ -1,39 +1,13 @@
-let express = require('express');
+let express = require("express");
 let app = express();
+console.log("hello world");
+module.exports = app;
 
+// app.get("/", function (req, res) {
+//   res.send("Hello Express");
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.get("/", function (req, res) {
+  let absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
