009b50f43cf8a2b633cf635a6a0b0fda73849bdf
jackkershaw <hello@jackkershaw.net>
2025-06-05 21:29:55 +0100

    chaining middleware to return request time

--- myApp.js
+++ myApp.js
@@ -1,13 +1,51 @@
 let express = require("express");
 let app = express();
-console.log("hello world");
 module.exports = app;
+require("dotenv").config();
+
+// middleware function - logging service
+
+app.use(function (req, res, next) {
+  let string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
+  next();
+});
+
+// send text response
 
 // app.get("/", function (req, res) {
 //   res.send("Hello Express");
 // });
 
+// file response
+
 app.get("/", function (req, res) {
   let absolutePath = __dirname + "/views/index.html";
   res.sendFile(absolutePath);
 });
+
+//json response
+
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/json", function (req, res) {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+});
+
+// chaining middleware
+
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.send({ time: req.time });
+  }
+);
+
+// echo server
